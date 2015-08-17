c5de5ddf9548920bb904f3b7dfd78abe6c9f94b2
gruppjo <jona_blubb@web.de>
2015-08-17 11:18:05 +0200

    lesson2 - js examples

--- lesson2JS/app/scripts/main.js
+++ lesson2JS/app/scripts/main.js
@@ -1,1 +1,50 @@
-console.log('\'Allo! \'Allo!');
+console.log('Lesson 2 - JavaScript');
+
+// DYNAMICALLY TYPED
+var number = 'hallo';
+console.log(number, typeof number);
+number = 1;
+console.log(number, typeof number);
+
+var car = {};
+console.log(car, typeof car);
+car.doors = 4;
+console.log(car, car.doors);
+
+console.log(Math.PI);
+var JonsNumber = 123874.123;
+Math.JonsNumber = JonsNumber;
+console.log(Math.JonsNumber);
+Math.PI = 3;
+console.log(2*Math.PI);
+
+
+// SYNTAX
+if (number === 1) {
+  console.log('yes, it\'s 1');
+}
+
+
+// FIRST CLASS FUNCTIONS
+// LAMBDA FUNCTIONS
+var add1 = function (x) {
+  return x + 1;
+};
+console.log(add1(number));
+
+var sayHello = function () {
+  console.log('hello');
+};
+var doStuff = function (func) {
+  return func();
+};
+doStuff(sayHello);
+doStuff(function () {
+  console.log('bye');
+});
+
+(function () {
+  console.log('hello!');
+})();
+
+// CALLBACK
