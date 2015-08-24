32377a518c1d9cd08cdd8e7643f728c5b8565ef7
gruppjo <jona_blubb@web.de>
2015-08-24 13:00:07 +0200

    webgl - mdn 3

--- lesson4Graphics/app/scripts/main.js
+++ lesson4Graphics/app/scripts/main.js
@@ -1,7 +1,23 @@
 console.log('\'Allo \'Allo!');
 
 var gl;
+var horizAspect = 480.0 / 640.0;
+
 var perspectiveMatrix;
+var vertexColorAttribute;
+var vertexPositionAttribute;
+var squareRotation = 0;
+var lastSquareUpdateTime;
+
+var mvMatrixStack = [];
+
+var squareXOffset = 0.0;
+var squareYOffset = 0.0;
+var squareZOffset = 0.0;
+
+var xIncValue = 0.2;
+var yIncValue = -0.4;
+var zIncValue = 0.3;
 
 function start () {
   var canvas = document.getElementById('glcanvas');
@@ -13,8 +29,11 @@ function start () {
     initBuffers();
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // set clear color to black, fully opaque
+    gl.clearDepth(1.0);
     gl.enable(gl.DEPTH_TEST); // enable depth testing
-    drawScene();
+    gl.depthFunc(gl.LEQUAL);
+
+    setInterval(drawScene, 15);
   }
 }
 
@@ -101,7 +120,6 @@ var getShader = function (gl, id) {
   return shader;
 };
 
-var horizAspect = 480.0 / 640.0;
 function initBuffers() {
   squareVerticesBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
@@ -134,6 +152,9 @@ function drawScene() {
 
   loadIdentity();
   mvTranslate([-0.0, 0.0, -6.0]);
+  mvPushMatrix();
+  mvRotate(squareRotation, [1, 0, 1]);
+  mvTranslate([squareXOffset, squareYOffset, squareZOffset]);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
   gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
@@ -141,6 +162,27 @@ function drawScene() {
   gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
   setMatrixUniforms();
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  mvPopMatrix();
+
+  var currentTime = Date.now();
+  if (lastSquareUpdateTime) {
+    var delta = currentTime - lastSquareUpdateTime;
+
+    squareRotation += (30 * delta) / 1000.0;
+
+    squareXOffset += xIncValue * ((30 * delta) / 1000.0);
+    squareYOffset += yIncValue * ((30 * delta) / 1000.0);
+    squareZOffset += zIncValue * ((30 * delta) / 1000.0);
+
+    if (Math.abs(squareYOffset) > 2.5) {
+      xIncValue = -xIncValue;
+      yIncValue = -yIncValue;
+      zIncValue = -zIncValue;
+    }
+  }
+
+  lastSquareUpdateTime = currentTime;
 }
 
 function loadIdentity() {
@@ -161,4 +203,29 @@ function setMatrixUniforms() {
 
   var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
   gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
+
+function mvPushMatrix(m) {
+  if (m) {
+    mvMatrixStack.push(m.dup());
+    mvMatrix = m.dup();
+  } else {
+    mvMatrixStack.push(mvMatrix.dup());
+  }
+}
+
+function mvPopMatrix() {
+  if (!mvMatrixStack.length) {
+    throw("Can't pop from an empty matrix stack.");
+  }
+
+  mvMatrix = mvMatrixStack.pop();
+  return mvMatrix;
+}
+
+function mvRotate(angle, v) {
+  var inRadians = angle * Math.PI / 180.0;
+
+  var m = Matrix.Rotation(inRadians, $V([v[0], v[1], v[2]])).ensure4x4();
+  multMatrix(m);
 }
